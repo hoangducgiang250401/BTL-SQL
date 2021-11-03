a83361e9bdeaf2ec97a45f179de95d1b871a066e
HDGiang <hoangducgiang250401@gmail.com>
2021-11-04 01:49:00 +0700

    test

--- js/trangchu.js
+++ js/trangchu.js
@@ -1,31 +1,68 @@
 $(document).ready(function () {
-    
-});
+  registered = $(".registered>tbody").find("tr").length;
+  $(".credis").append(registered);
 
-$(".checkbox").click(function (e) {
-    var mon = []
-    input = $(this).parents().eq(1).find('td');
-    $(input).each(function (index, value) {
-            mon.push($(value).text())
-    });
-    console.log(mon);
-    add_new(mon);
-});
-
-function add_new (mon){
-    var total_registered = $(".registered>tbody").find('tr').length+1;
-    var statu;
-    if(mon[7] == '' ){
-        mon[7] = 'đk lần đầu'
+  $(".checkbox").click(function (e) {
+      var mon = [];
+      input = $(this).parents().eq(1).find("td");
+      $(input).each(function (index, value) {
+        mon.push($(value).text());
+      });
+    if ($(this).prop("checked")) {
+      add_new(mon);
     }
-    $(".registered>tbody").append('<tr>\
-    <td id="stt">'+total_registered+'</td>\
-    <td>'+mon[1]+'</td>\
-    <td>'+mon[6]+'</td>\
-    <td>'+mon[2]+'</td>\
-    <td>'+mon[3]+'</td>\
-    <td>'+mon[8]+'</td>\
-    <td>Đk lần đầu</td>\
-    <td></td>\
-</tr>')
+    else{
+        drop_registered(mon);
+    }
+  });
+$('.trash').click(function (e) { 
+    e.preventDefault();
+    console.log('giang');
+});
+$('.trash').on('click', function () {
+    console.log(this);
+});
+$(document).on('click', '.trash', function() {
+    $(this).closest('tr').remove();
+})
+});
+
+function add_new(mon) {
+  if (mon[7] == "") {
+    mon[7] = "đk lần đầu";
+  }
+  $(".registered>tbody").append(
+    '<tr>\
+    <td id="stt" class = "center">' +
+      (registered + 1) +
+      "</td>\
+    <td>" +
+      mon[1] +
+      "</td>\
+    <td>" +
+      mon[6] +
+      "</td>\
+    <td>" +
+      mon[2] +
+      "</td>\
+    <td>" +
+      mon[3] +
+      "</td>\
+    <td>" +
+      mon[8] +
+      "</td>\
+    <td>" +
+      mon[7] +
+      '</td>\
+    <td class = "center"><i class="far fa-trash-alt trash" \
+    style="color: rgb(228, 55, 55); cursor: pointer;">\
+    </i></td>\
+    </tr>'
+  );
+  registered += 1;
+  $(".credis").text(registered);
 }
+function drop_registered(mon){
+    
+}
+// console.log(this);
